80b6e9d9b538c8c54b361fb0e4971556510d90a0
Samantha-Dutcher1986 <68967583+Samantha-Dutcher1986@users.noreply.github.com>
2020-09-18 23:41:48 -0500

    Some of the code is grayed out and I can't figure out why it's unreachable

--- index.js
+++ index.js
@@ -46,18 +46,57 @@ console.log(myAgeInDogYears(myAge, dogYears));
 // 11 - 15 lbs - 3% of their body weight
 // > 15lbs - 2% of their body weight
 
+// function results(final){
+//   console.log("Adult Dog Needs", final, "lbs of food");
+// }
+// let lbs = 1;
+function adultDog(lbs) {
+    if (lbs <= 5){
+      return(lbs * .05);
+  }
+  else if (lbs <= 10) {
+      return(lbs * .04);
+  }
+  else if (lbs <= 15){
+      return(lbs * .03);
+  }
+  else{
+      return (lbs * .02);
+  }
+}
+// console.log(adultDog(lbs));
+
+
 // Puppies less than 1 year
 // 2 - 4 months 10% of their body weight
 // 4 - 7 months 5% of their body weight
 // 7 - 12 months 4% of their body weight
 
+// let age = 0.25;
+function puppies(age,lbs){
+  if (age >= .167 && age <= .33){
+      return(lbs * .1);
+  }
+  else if(age > .33 && age <=.58){
+      return(lbs *.05);
+  }
+  else {
+      return(lbs * .04);
+  }
+// }console.log(puppies(age,lbs));
+
+function foodcalculator(age, lbs){
+  if(age < 1){
+    return (puppies(age, lbs));
+  }
+  else{
+    return(adult(lbs));
+  }
+}
+
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
 
-function DogFeeder(age, weight){
-  for(let age = 0; age <=1; age++ && weight = 0; weight <= 5%; weight++){
-  return age * weight;
-  console.log(DogFeeder(age, weight));
-}
+foodcalculator(1,15); //Don't know why this is grayed out when it's functional in the code above
 
 /************************************************************** Task 4 **************************************************************/
 // Rock, Paper, Sissors
@@ -66,17 +105,59 @@ function DogFeeder(age, weight){
 // use math.random to determine the computers choice
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number
 
+function RPS(){ //Not sure why RPS is grayed out
+  let random = Math.random()
+  if (random < .3) {
+    console.log("Rock") 
+  }
+  if (random > .3 && random < .6) {
+    console.log("Paper")
+  }
+  if (random > .6) {
+    console.log("Scissors")
+  }
+console.log(RPS);
+}
 /************************************************************** Task 5 **************************************************************/
 //Metric Converter
 //a. KM to Miles - should take the number of kilometers and convert it to the equal number of miles
 
+let km = 1;
+function convert(km){
+  return km / 1.609; 
+} console.log(convert(km)); //Why is this unreachable?
+
 //b. Feet to CM - should take the number of feet and convert it to the equal number of centimeters
+let feet = 1;
+function convertFT(feet){
+  return feet * 30.48;
+}console.log(convertFT(feet)); 
 
 /************************************************************** Task 6 **************************************************************/
 // 99 bottles of soda on the wall
 // create a function called annoyingSong
 // the function should take a starting number as an argument and count down - at each iteration it should log (number) bottles of soda on the wall, (number) bottles of soda, take one down pass it around (number left over) bottles of soda on the wall`
 
+var bottles;
+for (counter = 99; counter >= 1; counter = counter - 1) 
+{
+    if (counter == 1) {
+        bottles = 'bottle';
+    } else {
+        bottles = 'bottles';
+    }
+    console.log(counter+" "+bottles+" of beer on the wall.");
+    if (counter < 99) {
+        console.log("");
+        console.log(counter+" "+bottles+" of beer on the wall.");
+    }
+    console.log(counter+" "+bottles+" of beer.");
+    console.log("Take one down.");
+    console.log("Pass it around.");
+    if (counter == 1) {
+        console.log("No bottles of beer on the wall.");
+    }
+}
 /************************************************************** Task 7 **************************************************************/
 //Grade Calculator
 //write a javaScript program that takes a mark out of 100 and returns a corisponding letter grade
@@ -86,11 +167,45 @@ function DogFeeder(age, weight){
 //60s should be D
 //and anything below 60 should be F
 
+function assignGrade(score) {
+  if (score > 90) {
+      return 'A';
+  } else if (score > 80) {
+      return 'B';
+  } else if (score > 70) {
+      return 'C';
+  } else if (score > 65) {
+      return 'D';
+  } else {
+      return 'F';
+  }
+}
+
+for (let i = 80; i <= 100; i++) {
+console.log('For ' + i + ', you got a ' + assignGrade(i));
+}
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
 // Hint - you may need to study tomorrow's traning kit on arrays
 // try looking up the .includes() method
 
+function vowel_count(str1)
+{
+  var vowel_list = 'aeiouAEIOU';
+  var vcount = 0;
+  
+  for(var x = 0; x < str1.length ; x++)
+  {
+    if (vowel_list.indexOf(str1[x]) !== -1)
+    {
+      vcount += 1;
+    }
+  
+  }
+  return vcount;
+}
+console.log(vowel_count("The quick brown fox"));
+
 /************************************************************** Stretch **************************************************************/
 //Take Rock, Paper, Sissors further
-//update your rock papers sissors code below to take a prompt from a user using the window object
+//update your rock papers sissors code below to take a prompt from a user using the window object./
